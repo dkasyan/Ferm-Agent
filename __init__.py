042244101b391d91e2c29e7efd11fb53e81f1614
Damian Kasyan <damian.kasyan@hotmail.com>
2021-09-08 16:15:03 +0200

    Add init

--- __init__.py
+++ __init__.py
@@ -1,3 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+if __name__ == '__main__':
+    app.run()
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "Entry": models.Entry
